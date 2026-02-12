514212057227512488e8c92666e1c25ebe7b756d
Klamkin, Michael <mklamkin3@gatech.edu>
2026-02-11 23:18:46 -0500

    simplify

--- ext/MadNLPMOI/MOI_parametric.jl
+++ ext/MadNLPMOI/MOI_parametric.jl
@@ -52,11 +52,36 @@ end
 _n_qp(model::Optimizer) = length(model.qp_data.constraints)
 _n_nlp(model::Optimizer) = length(model.nlp_data.constraint_bounds)
 
-function _has_nlp_expressions(model::Optimizer)
-    return model.nlp_model !== nothing && (
-        _n_nlp(model) > 0 || model.nlp_model.objective !== nothing
+function _has_nlp_constraints(model::Optimizer)
+    return model.param_evaluator !== nothing && _n_nlp(model) > 0
+end
+
+function _has_nlp_objective(model::Optimizer)
+    return model.param_evaluator !== nothing &&
+           model.nlp_model !== nothing &&
+           model.nlp_model.objective !== nothing
+end
+
+_has_nlp_lagrangian(model::Optimizer) = _has_nlp_constraints(model) || _has_nlp_objective(model)
+
+function _quad_term_indices(term, model)
+    vi, vj = term.variable_1, term.variable_2
+    return (
+        coef = term.coefficient,
+        pi = _get_param_idx(model, vi),
+        pj = _get_param_idx(model, vj),
+        xi = _get_primal_idx(model, vi),
+        xj = _get_primal_idx(model, vj),
     )
 end
+
+function _get_param_value(model, pidx)
+    return model.nlp_model[model.parameters[model.param_order[pidx]]]
+end
+
+abstract type DiffDirection end
+struct ForwardMode <: DiffDirection end
+struct ReverseMode <: DiffDirection end
 
 function ParametricNLPModels.jpprod!(
     nlp::MOIModel{T}, x::AbstractVector, v::AbstractVector, Jv::AbstractVector,
@@ -67,10 +92,10 @@ function ParametricNLPModels.jpprod!(
     n_nlp_c = _n_nlp(model)
 
     for (row, constraint) in enumerate(model.qp_data.constraints)
-        _jpprod_qp_constraint!(Jv, constraint, model, x, v, row)
+        _jprod_qp!(ForwardMode(), Jv, constraint, model, x, v, row)
     end
 
-    if _has_nlp_expressions(model) && model.param_evaluator !== nothing
+    if _has_nlp_constraints(model)
         n_x = length(x)
         n_p = model.param_n_p
         _fill_x_combined!(model, x)
@@ -79,50 +104,11 @@ function ParametricNLPModels.jpprod!(
         fill!(ve, zero(T))
         ve[n_x+1:n_x+n_p] .= v
 
-        if n_nlp_c > 0
-            jvp_result = similar(Jv, n_nlp_c)
-            MOI.eval_constraint_jacobian_product(model.param_evaluator, jvp_result, model.param_x_combined, ve)
-            Jv[n_qp+1:n_qp+n_nlp_c] .+= jvp_result
-        end
+        Jv_nlp = view(Jv, n_qp+1:n_qp+n_nlp_c)
+        MOI.eval_constraint_jacobian_product(model.param_evaluator, Jv_nlp, model.param_x_combined, ve)
     end
 
     return Jv
-end
-
-function _jpprod_qp_constraint!(Jv, f::MOI.ScalarAffineFunction, model, x, v, row)
-    for term in f.terms
-        i = _get_param_idx(model, term.variable)
-        !iszero(i) && (Jv[row] += term.coefficient * v[i])
-    end
-    return
-end
-
-function _jpprod_qp_constraint!(Jv, f::MOI.ScalarQuadraticFunction, model, x, v, row)
-    for term in f.affine_terms
-        i = _get_param_idx(model, term.variable)
-        !iszero(i) && (Jv[row] += term.coefficient * v[i])
-    end
-    for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
-        !iszero(pi) && !iszero(xj) && (Jv[row] += coef * x[xj] * v[pi])
-        !iszero(pj) && !iszero(xi) && (Jv[row] += coef * x[xi] * v[pj])
-        if !iszero(pi) && !iszero(pj)
-            p_vals_i = model.nlp_model[model.parameters[model.param_order[pi]]]
-            p_vals_j = model.nlp_model[model.parameters[model.param_order[pj]]]
-            if pi == pj
-                Jv[row] += coef * p_vals_i * v[pi]
-            else
-                Jv[row] += coef * p_vals_j * v[pi]
-                Jv[row] += coef * p_vals_i * v[pj]
-            end
-        end
-    end
-    return
 end
 
 function ParametricNLPModels.jptprod!(
@@ -134,59 +120,78 @@ function ParametricNLPModels.jptprod!(
     n_nlp_c = _n_nlp(model)
 
     for (row, constraint) in enumerate(model.qp_data.constraints)
-        _jptprod_qp_constraint!(Jtv, constraint, model, x, v[row], row)
+        _jprod_qp!(ReverseMode(), Jtv, constraint, model, x, v, row)
     end
 
-    if _has_nlp_expressions(model) && model.param_evaluator !== nothing
+    if _has_nlp_constraints(model)
         n_x = length(x)
         n_p = model.param_n_p
         _fill_x_combined!(model, x)
 
-        if n_nlp_c > 0
-            v_nlp = view(v, n_qp+1:n_qp+n_nlp_c)
-            result = model.param_result
-            MOI.eval_constraint_jacobian_transpose_product(model.param_evaluator, result, model.param_x_combined, v_nlp)
-            Jtv .+= view(result, n_x+1:n_x+n_p)
-        end
+        v_nlp = view(v, n_qp+1:n_qp+n_nlp_c)
+        result = model.param_result
+        MOI.eval_constraint_jacobian_transpose_product(model.param_evaluator, result, model.param_x_combined, v_nlp)
+        Jtv .+= view(result, n_x+1:n_x+n_p)
     end
 
     return Jtv
 end
 
-function _jptprod_qp_constraint!(Jtv, f::MOI.ScalarAffineFunction, model, x, v_row, row)
+function _jprod_qp!(::ForwardMode, Jv, f::MOI.ScalarAffineFunction, model, x, v, row)
+    for term in f.terms
+        i = _get_param_idx(model, term.variable)
+        !iszero(i) && (Jv[row] += term.coefficient * v[i])
+    end
+end
+
+function _jprod_qp!(::ForwardMode, Jv, f::MOI.ScalarQuadraticFunction, model, x, v, row)
+    for term in f.affine_terms
+        i = _get_param_idx(model, term.variable)
+        !iszero(i) && (Jv[row] += term.coefficient * v[i])
+    end
+    for term in f.quadratic_terms
+        (; coef, pi, pj, xi, xj) = _quad_term_indices(term, model)
+        !iszero(pi) && !iszero(xj) && (Jv[row] += coef * x[xj] * v[pi])
+        !iszero(pj) && !iszero(xi) && (Jv[row] += coef * x[xi] * v[pj])
+        if !iszero(pi) && !iszero(pj)
+            p_i, p_j = _get_param_value(model, pi), _get_param_value(model, pj)
+            if pi == pj
+                Jv[row] += coef * p_i * v[pi]
+            else
+                Jv[row] += coef * p_j * v[pi] + coef * p_i * v[pj]
+            end
+        end
+    end
+end
+
+function _jprod_qp!(::ReverseMode, Jtv, f::MOI.ScalarAffineFunction, model, x, v, row)
+    v_row = v[row]
     for term in f.terms
         i = _get_param_idx(model, term.variable)
         !iszero(i) && (Jtv[i] += term.coefficient * v_row)
     end
-    return
 end
 
-function _jptprod_qp_constraint!(Jtv, f::MOI.ScalarQuadraticFunction, model, x, v_row, row)
+function _jprod_qp!(::ReverseMode, Jtv, f::MOI.ScalarQuadraticFunction, model, x, v, row)
+    v_row = v[row]
     for term in f.affine_terms
         i = _get_param_idx(model, term.variable)
         !iszero(i) && (Jtv[i] += term.coefficient * v_row)
     end
     for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
+        (; coef, pi, pj, xi, xj) = _quad_term_indices(term, model)
         !iszero(pi) && !iszero(xj) && (Jtv[pi] += coef * x[xj] * v_row)
         !iszero(pj) && !iszero(xi) && (Jtv[pj] += coef * x[xi] * v_row)
         if !iszero(pi) && !iszero(pj)
-            p_vals_i = model.nlp_model[model.parameters[model.param_order[pi]]]
-            p_vals_j = model.nlp_model[model.parameters[model.param_order[pj]]]
+            p_i, p_j = _get_param_value(model, pi), _get_param_value(model, pj)
             if pi == pj
-                Jtv[pi] += coef * p_vals_i * v_row
+                Jtv[pi] += coef * p_i * v_row
             else
-                Jtv[pi] += coef * p_vals_j * v_row
-                Jtv[pj] += coef * p_vals_i * v_row
+                Jtv[pi] += coef * p_j * v_row
+                Jtv[pj] += coef * p_i * v_row
             end
         end
     end
-    return
 end
 
 function ParametricNLPModels.hpprod!(
@@ -196,15 +201,14 @@ function ParametricNLPModels.hpprod!(
     model = nlp.model
     fill!(Hv, zero(T))
     n_qp = _n_qp(model)
-
     σ = T(obj_weight)
 
-    _hpprod_qp_obj!(Hv, model.qp_data.objective, model, v, σ)
+    _hprod_qp!(ForwardMode(), Hv, model.qp_data.objective, model, v, σ)
     for (row, constraint) in enumerate(model.qp_data.constraints)
-        _hpprod_qp_constraint!(Hv, constraint, model, v, y[row])
+        _hprod_qp!(ForwardMode(), Hv, constraint, model, v, y[row])
     end
 
-    if _has_nlp_expressions(model) && model.param_evaluator !== nothing
+    if _has_nlp_lagrangian(model)
         n_x = length(x)
         n_p = model.param_n_p
         n_nlp_c = _n_nlp(model)
@@ -224,42 +228,6 @@ function ParametricNLPModels.hpprod!(
     return Hv
 end
 
-function _hpprod_qp_obj!(Hv, f::MOI.ScalarAffineFunction, model, v, σ)
-    return
-end
-
-function _hpprod_qp_obj!(Hv, f::MOI.ScalarQuadraticFunction, model, v, σ)
-    for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
-        !iszero(pi) && !iszero(xj) && (Hv[xj] += σ * coef * v[pi])
-        !iszero(pj) && !iszero(xi) && (Hv[xi] += σ * coef * v[pj])
-    end
-    return
-end
-
-function _hpprod_qp_constraint!(Hv, f::MOI.ScalarAffineFunction, model, v, scale)
-    return
-end
-
-function _hpprod_qp_constraint!(Hv, f::MOI.ScalarQuadraticFunction, model, v, scale)
-    for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
-        !iszero(pi) && !iszero(xj) && (Hv[xj] += scale * coef * v[pi])
-        !iszero(pj) && !iszero(xi) && (Hv[xi] += scale * coef * v[pj])
-    end
-    return
-end
-
 function ParametricNLPModels.hptprod!(
     nlp::MOIModel{T}, x::AbstractVector, y::AbstractVector,
     v::AbstractVector, Htv::AbstractVector; obj_weight::Real = one(T),
@@ -267,15 +235,14 @@ function ParametricNLPModels.hptprod!(
     model = nlp.model
     fill!(Htv, zero(T))
     n_qp = _n_qp(model)
-
     σ = T(obj_weight)
 
-    _hptprod_qp_obj!(Htv, model.qp_data.objective, model, v, σ)
+    _hprod_qp!(ReverseMode(), Htv, model.qp_data.objective, model, v, σ)
     for (row, constraint) in enumerate(model.qp_data.constraints)
-        _hptprod_qp_constraint!(Htv, constraint, model, v, y[row])
+        _hprod_qp!(ReverseMode(), Htv, constraint, model, v, y[row])
     end
 
-    if _has_nlp_expressions(model) && model.param_evaluator !== nothing
+    if _has_nlp_lagrangian(model)
         n_x = length(x)
         n_p = model.param_n_p
         n_nlp_c = _n_nlp(model)
@@ -295,40 +262,22 @@ function ParametricNLPModels.hptprod!(
     return Htv
 end
 
-function _hptprod_qp_obj!(Htv, f::MOI.ScalarAffineFunction, model, v, σ)
-    return
-end
+_hprod_qp!(::DiffDirection, output, ::MOI.ScalarAffineFunction, model, v, scale) = nothing
 
-function _hptprod_qp_obj!(Htv, f::MOI.ScalarQuadraticFunction, model, v, σ)
+function _hprod_qp!(::ForwardMode, Hv, f::MOI.ScalarQuadraticFunction, model, v, scale)
     for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
-        !iszero(pi) && !iszero(xj) && (Htv[pi] += σ * coef * v[xj])
-        !iszero(pj) && !iszero(xi) && (Htv[pj] += σ * coef * v[xi])
+        (; coef, pi, pj, xi, xj) = _quad_term_indices(term, model)
+        !iszero(pi) && !iszero(xj) && (Hv[xj] += scale * coef * v[pi])
+        !iszero(pj) && !iszero(xi) && (Hv[xi] += scale * coef * v[pj])
     end
-    return
 end
 
-function _hptprod_qp_constraint!(Htv, f::MOI.ScalarAffineFunction, model, v, scale)
-    return
-end
-
-function _hptprod_qp_constraint!(Htv, f::MOI.ScalarQuadraticFunction, model, v, scale)
+function _hprod_qp!(::ReverseMode, Htv, f::MOI.ScalarQuadraticFunction, model, v, scale)
     for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
+        (; coef, pi, pj, xi, xj) = _quad_term_indices(term, model)
         !iszero(pi) && !iszero(xj) && (Htv[pi] += scale * coef * v[xj])
         !iszero(pj) && !iszero(xi) && (Htv[pj] += scale * coef * v[xi])
     end
-    return
 end
 
 function ParametricNLPModels.grad_param!(
@@ -337,9 +286,9 @@ function ParametricNLPModels.grad_param!(
     model = nlp.model
     fill!(g, zero(T))
 
-    _grad_param_qp_obj!(g, model.qp_data.objective, model, x)
+    _grad_param_qp!(g, model.qp_data.objective, model, x)
 
-    if _has_nlp_expressions(model) && model.param_evaluator !== nothing
+    if _has_nlp_objective(model)
         n_x = length(x)
         n_p = model.param_n_p
         _fill_x_combined!(model, x)
@@ -353,40 +302,33 @@ function ParametricNLPModels.grad_param!(
     return g
 end
 
-function _grad_param_qp_obj!(g, f::MOI.ScalarAffineFunction, model, x)
+function _grad_param_qp!(g, f::MOI.ScalarAffineFunction, model, x)
     for term in f.terms
         i = _get_param_idx(model, term.variable)
         !iszero(i) && (g[i] += term.coefficient)
     end
-    return
 end
 
-function _grad_param_qp_obj!(g, f::MOI.ScalarQuadraticFunction, model, x)
+function _grad_param_qp!(g, f::MOI.ScalarQuadraticFunction, model, x)
     for term in f.affine_terms
         i = _get_param_idx(model, term.variable)
         !iszero(i) && (g[i] += term.coefficient)
     end
 
     for term in f.quadratic_terms
-        vi, vj = term.variable_1, term.variable_2
-        pi = _get_param_idx(model, vi)
-        pj = _get_param_idx(model, vj)
-        xi = _get_primal_idx(model, vi)
-        xj = _get_primal_idx(model, vj)
-        coef = term.coefficient
+        (; coef, pi, pj, xi, xj) = _quad_term_indices(term, model)
 
         !iszero(pi) && !iszero(xj) && (g[pi] += coef * x[xj])
         !iszero(pj) && !iszero(xi) && (g[pj] += coef * x[xi])
+
         if !iszero(pi) && !iszero(pj)
-            p_vals_i = model.nlp_model[model.parameters[model.param_order[pi]]]
-            p_vals_j = model.nlp_model[model.parameters[model.param_order[pj]]]
+            p_i, p_j = _get_param_value(model, pi), _get_param_value(model, pj)
             if pi == pj
-                g[pi] += coef * p_vals_i
+                g[pi] += coef * p_i
             else
-                g[pi] += coef * p_vals_j
-                g[pj] += coef * p_vals_i
+                g[pi] += coef * p_j
+                g[pj] += coef * p_i
             end
         end
     end
-    return
 end
